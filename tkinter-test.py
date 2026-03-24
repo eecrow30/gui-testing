77fd2ec2db0f86611c5e699f71f16705fcf1ce74
Erin <26493101@edgehill.ac.uk>
2026-03-24 09:12:15 +0000

    Initialize Tkinter window for To-Do List
    Set up basic Tkinter window for a To-Do List application.

--- tkinter-test.py
+++ tkinter-test.py
@@ -1,2 +1,21 @@
 
 import tkinter as tk
+from tkinter import messagebox
+
+
+
+root = tk.Tk()
+root.title("To-Do List")
+root.geometry("420x480")
+root.configure(bg='pale green')
+
+tk.Label(root,text="Input a task and click [ADD]:",
+         fg="forest green",     ## instruction label so user knows what to do
+         font=("Comic Sans MS",11)).pack(pady=(20,5))
+
+
+
+
+
+
+root.mainloop()
